87e744afc8501ad7e9ba970397b094fb373cd6d9
Sami Salih İbrahimbas <info@ssibrahimbas.com>
2022-09-15 12:02:08 +0300

    feat(pino-logger): files exported

--- packages/pino-logger/lib/index.ts
+++ packages/pino-logger/lib/index.ts
@@ -3,3 +3,12 @@
  * Copyright(c) 2022 Sami Salih İBRAHİMBAŞ
  * MIT Licensed
  */
+
+export * from "./module/pino.enum";
+export * from "./module/pino.module";
+export * from "./module/pino.service";
+export * from "./hooks/pino.hooks";
+export * from "./hooks/http-middleware.hooks";
+export * from "./hooks/ws-middleware.hooks";
+export * from "./app/app";
+export * from "./middlewares/middleware.types";
